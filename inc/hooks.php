9bd56f01fb3dcad3095e2cf63d9700067560f139
Pea <pea@misfist.com>
2022-09-04 01:18:33 -0400

    Add archive title filter

--- inc/hooks.php
+++ inc/hooks.php
@@ -28,6 +28,31 @@ function query_posts( object $query ) {
 \add_filter( 'pre_get_posts', __NAMESPACE__ . '\query_posts', 11 );
 
 /**
+ * Remove text before archive 
+ *
+ * @see https://developer.wordpress.org/reference/hooks/get_the_archive_title/
+ * 
+ * @param string $title
+ * @return string $title
+ */
+function archive_title( $title ) {
+    if ( \is_category() ) {
+        $title = \single_cat_title( '', false );
+    } elseif ( \is_tag() ) {
+        $title = \single_tag_title( '', false );
+    } elseif ( \is_author() ) {
+        $title = '<span class="vcard">' . \get_the_author() . '</span>';
+    } elseif ( \is_post_type_archive() ) {
+        $title = \post_type_archive_title( '', false );
+    } elseif ( \is_tax() ) {
+        $title = \single_term_title( '', false );
+    }
+  
+    return $title;
+}
+\add_filter( 'get_the_archive_title', __NAMESPACE__ . '\archive_title' );
+
+/**
  * Modify the content
  *
  * @param string $content
